a1dafeaadcb899d2235358d7726d4f8779beb54d
manjaro23 <152917540+manjaro23@users.noreply.github.com>
2024-03-01 08:43:05 +0300

    Add files via upload

---
+++ src/app/forms/MainForm.php
@@ -0,0 +1,193 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class MainForm extends AbstractForm
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * @event circle5.click-Left 
+     */
+    function doCircle5ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle.click-Left 
+     */
+    function doCircleClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circleAlt.click-Left 
+     */
+    function doCircleAltClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle3.click-Left 
+     */
+    function doCircle3ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle4.click-Left 
+     */
+    function doCircle4ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle6.click-Left 
+     */
+    function doCircle6ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle7.click-Left 
+     */
+    function doCircle7ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle8.click-Left 
+     */
+    function doCircle8ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle9.click-Left 
+     */
+    function doCircle9ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle10.click-Left 
+     */
+    function doCircle10ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle11.click-Left 
+     */
+    function doCircle11ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle12.click-Left 
+     */
+    function doCircle12ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle13.click-Left 
+     */
+    function doCircle13ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle14.click-Left 
+     */
+    function doCircle14ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+    /**
+     * @event circle15.click-Left 
+     */
+    function doCircle15ClickLeft(UXMouseEvent $e = null)
+    {    
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
