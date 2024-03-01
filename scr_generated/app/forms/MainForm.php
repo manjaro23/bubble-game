26ddd70a61ff831a09e7c9e3e5ca71b2dcd37985
manjaro23 <152917540+manjaro23@users.noreply.github.com>
2024-03-01 08:41:53 +0300

    Add files via upload

---
+++ scr_generated/app/forms/MainForm.php
@@ -0,0 +1,298 @@
+<?php
+namespace app\forms;
+
+use std, gui, framework, app;
+
+
+class MainForm extends AbstractForm
+{
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    /**
+     * @event circle5.click-Left 
+     */
+    function doCircle5ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle.click-Left 
+     */
+    function doCircleClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circleAlt.click-Left 
+     */
+    function doCircleAltClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle3.click-Left 
+     */
+    function doCircle3ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle4.click-Left 
+     */
+    function doCircle4ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle6.click-Left 
+     */
+    function doCircle6ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle7.click-Left 
+     */
+    function doCircle7ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle8.click-Left 
+     */
+    function doCircle8ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle9.click-Left 
+     */
+    function doCircle9ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle10.click-Left 
+     */
+    function doCircle10ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle11.click-Left 
+     */
+    function doCircle11ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle12.click-Left 
+     */
+    function doCircle12ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle13.click-Left 
+     */
+    function doCircle13ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle14.click-Left 
+     */
+    function doCircle14ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+    /**
+     * @event circle15.click-Left 
+     */
+    function doCircle15ClickLeft(UXMouseEvent $e = null)
+    {    
+$e = $event ?: $e; // legacy code from 16 rc-2
+
+		$e->sender->hide();
+		waitAsync(1000, function () use ($e, $event) {
+			$e->sender->show();
+		});
+
+        
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
